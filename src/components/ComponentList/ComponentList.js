50b71c9d645b1f8f1a56883579d0db82e3b247f8
Adi Vrskic <adivrskic123@gmail.com>
2023-02-16 01:03:26 -0500

    start examples page

--- src/components/ComponentList/ComponentList.js
+++ src/components/ComponentList/ComponentList.js
@@ -3,11 +3,17 @@ import "./componentlist.scss";
 import { Link } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import { StateContext } from "../../context/StateProvider";
+import { Alert, Badge, Button } from "@adivrskic/pillow";
 
 const ComponentList = () => {
   const [{ sidebarOpen }] = React.useContext(StateContext);
 
-  const components = ["Alert", "Button"];
+  const components = {
+    Alert: Alert,
+    Badge: Badge,
+    Button: Button,
+  };
+
   const toKebabCase = (string) => {
     return string
       .replace(/([a-z])([A-Z])/g, "$1-$2")
@@ -17,15 +23,45 @@ const ComponentList = () => {
 
   return (
     <div className="pillow-components">
-      <Sidebar header={"Components"} page={"components"} items={components} />
+      <Sidebar
+        header={"Components"}
+        page={"components"}
+        items={Object.keys(components)}
+      />
       <div
         className={`pillow-components__content ${sidebarOpen ? "open" : ""}`}
       >
         <h2>Components</h2>
         <div className="pillow-components__content-items">
-          {components.map((component) => (
-            <Link to={`/components/${toKebabCase(component)}`}>
-              <div className="pillow-components__content-item">{component}</div>
+          {Object.keys(components).map((component) => (
+            <Link
+              className="pillow-components__content-item"
+              to={{
+                pathname: `/components/${toKebabCase(component)}`,
+                state: { component: component },
+              }}
+            >
+              <h4>{component}</h4>
+              <div className="pillow-components__content-item-body">
+                {component === "Alert" && (
+                  <Alert
+                    heading="Header"
+                    severity="info"
+                    label="Pillow UI Alert Body"
+                    variant="flat"
+                    role="button"
+                  />
+                )}
+                {component === "Badge" && <Badge content="9" variant="flat" />}
+                {component === "Button" && (
+                  <Button
+                    label="Pillow UI Button"
+                    variant="flat"
+                    size="small"
+                    role="button"
+                  />
+                )}
+              </div>
             </Link>
           ))}
         </div>
--- src/components/Example/Example.js
+++ src/components/Example/Example.js
@@ -1,14 +1,24 @@
 import React from "react";
 import "./example.scss";
+import { useLocation } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
+import { Alert, Badge, Button } from "@adivrskic/pillow";
 
 const Example = () => {
-  const components = ["Alert", "Button"];
+  const components = ["Alert", "Badge", "Button"];
+  const location = useLocation();
+  const Component =
+    location.pathname.split("/").pop()[0].toUpperCase() +
+    location.pathname.split("/").pop().slice(1);
 
   return (
     <div>
       <Sidebar header={"Components"} page={"components"} items={components} />
-      Example
+      <div className="pillow-example">
+        <div className="pillow-example__content">
+          <h2>{Component}</h2>
+        </div>
+      </div>
     </div>
   );
 };
